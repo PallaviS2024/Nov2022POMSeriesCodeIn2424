5d4559f73cd420dadbb007587818c92f01cfe9d4
unknown <mlokpal17@gmail.com>
2024-06-09 21:35:33 +0530

    changes made as per Naveen PR review 9May2024

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -1,6 +1,7 @@
 //This ia a dummy class for git tutorial 10 git branching naveen
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import com.qa.opencart.utils.ElementUtil;
 
@@ -8,11 +9,22 @@ public class OrderPage {
 	// 1
 	private WebDriver driver;
 	private ElementUtil eleUtil;
-
-	// 3
+	
+	//locators
+	private By loc = By.id("order");
+	private By price = By.id("price");
+	
 	public OrderPage(WebDriver driver){
 			this.driver = driver;
 			eleUtil = new ElementUtil(driver);
+	}
+	
+	public void getOrder() {
+		System.out.println("Order plz");
+	}
+	
+	public void getPrice() {
+		System.out.println("Price plz");
 	}
 
 }
