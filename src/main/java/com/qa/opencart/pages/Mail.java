eeb807335c2c7e912441b3f993d706d9ffdc798a
PallaviS2024 <mlokpal17@gmail.com>
2024-06-10 16:28:29 +0530

    Create Mail.java
    merging concept practice Naveen Auto Tuts

---
+++ src/main/java/com/qa/opencart/pages/Mail.java
@@ -0,0 +1,18 @@
+//This ia a dummy class for git tutorial 10 git merging naveen
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import com.qa.opencart.utils.ElementUtil;
+
+public class Mail{
+	
+	public void getSendersList() {
+		System.out.println("get Senders List please!!!");
+	}
+	
+	// public void getPrice() {
+	// 	System.out.println("Price plz");
+	// }
+
+}
