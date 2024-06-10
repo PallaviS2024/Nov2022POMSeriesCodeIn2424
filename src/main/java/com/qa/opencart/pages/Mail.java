abfe5a1d7b390f30786c8e9f4228acc330e19a2f
unknown <mlokpal17@gmail.com>
2024-06-10 16:44:58 +0530

    Pallavi Mail java class created

---
+++ src/main/java/com/qa/opencart/pages/Mail.java
@@ -0,0 +1,18 @@
+//This ia a dummy class for git tutorial 10 git merging naveen
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import com.qa.opencart.utils.ElementUtil;
+
+public class Mail{
+	
+	public void getSendersList() {
+		System.out.println("From Pallavi Eclipse Workspace get Senders List please!!!");
+	}
+	
+	 public void getEmailBody() {
+	 	System.out.println("From Pallavi Eclipse Workspace get Email Body please!!!");
+	 }
+
+}
